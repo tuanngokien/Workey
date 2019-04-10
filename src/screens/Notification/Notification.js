5f274e569b5095616d83fa245fecf9128cb8b68c
sotatek-thiennguyen <thiennguyen@sotatek.com>
2019-04-10 23:53:05 +0700

    addNoti

--- src/screens/Notification/Notification.js
+++ src/screens/Notification/Notification.js
@@ -1,12 +1,107 @@
 import React from "react";
-import {Text} from "react-native"
-
+import {
+    ScrollView,
+    View,
+    TouchableOpacity,
+    StyleSheet,
+    Text,
+    FlatList
+} from 'react-native';
+import TopNavigator from "../../components/TopNavigator/TopNavigator"
+import Notification from "../Notification/data"
+import { Avatar } from "react-native-elements"
+let styles=StyleSheet.create({
+    containerList:{
+        backgroundColor: "white",
+        paddingTop: 5,
+    },
+    titleContainer: {
+        fontFamily: "Nunito-Bold",
+        fontSize: 20,
+        color: "#000000",
+        paddingVertical: "1%",
+        paddingLeft:"2%",
+        left:0
+        
+    }
+})
 class NotificationScreen extends React.Component {
+    state={
+        notification: Notification
+    }
+    handleMessage(message) {
+        if (message.length > 30) {
+            let tmp = message.slice(0, 30) + "...";
+            return (
+                <Text>{tmp}</Text>
+            )
+        }
+        else {
+            return (
+                <Text>{message}</Text>
+            )
+        }
+    }
+    renderNoti = ({ item }) => {
+        return(
+            <TouchableOpacity style={{backgroundColor:"white",marginVertical:10,heigh:80}}>
+                <View style={{flexDirection:"row"}}>
+                    <View style={{width: "15%"}}>
+                        <Avatar 
+                            rounded
+                            source={{
+                                uri: 'https://s3.amazonaws.com/uifaces/faces/twitter/adhamdannaway/128.jpg'
+                            }}
+                            size={50}
+                            containerStyle={{
+                                borderRadius:100,
+                                borderWidth: 1.5,
+                                borderColor: "white"
+                            }}
+                        />
+                    </View>
+                    <View style={{width: "65%"}}>
+                            <Text style={{fontSize: 17,color:"black"}}>
+                                <Text style={{fontWeight:"bold"}}>{item.user.name}</Text>
+                                {this.handleMessage(item.noti)}
+                            </Text>
+                    </View>
+                    <View style={{width: "20%",right:0,bottom:0,position:"absolute"}}>
+                            <Text>{item.user.time}</Text>
+                    </View>
+                </View>
+            </TouchableOpacity>
+        )
+    }
+    
     render() {
         return (
-            <Text>
-                Notification
-            </Text>
+            <ScrollView style={{backgroundColor:"transparent"}}>
+                <View >
+                <TopNavigator
+                    containerStyle={{
+                        //backgroundColor: "transparent",
+                        paddingTop: 20
+                    }}
+                    titleStyle={{
+                        fontSize: 15,
+                        fontFamily: ""
+                    }}/>
+                </View>
+                
+                <View>
+                    <Text style={styles.titleContainer}>Notification</Text>
+                </View>
+                <View style={styles.containerList}>
+                    <FlatList
+                        data={this.state.notification}
+                        renderItem={this.renderNoti}
+                        //keyExtractor={(item,index)=>item.user.id.toString()}
+                    />
+                </View>
+            
+            </ScrollView>
+            
         );
     }
 }
---
+++ src/screens/Notification/data.js
@@ -0,0 +1,139 @@
+let avatar4 = require("../../assets/images/avatar/avatar4.jpg");
+var Notification = [
+    {
+        user:{
+            id: 0,
+            name: "SamSungRnD",
+            avatar : "https://s3.amazonaws.com/uifaces/faces/twitter/adhamdannaway/128.jpg",
+            time: "1 hour ago"
+        },
+        noti:" has approve your CV. "
+    },
+    {
+        user:{
+            id:1,
+            name: "Viettel Telecom",
+            avatar : "https://s3.amazonaws.com/uifaces/faces/twitter/adhamdannaway/128.jpg",
+            time: "1 hour ago"
+        },
+        noti:" has send you a invitation to interview "
+    },
+    {
+        user:{
+            id:2,
+            name: "Sotatek",
+            avatar : "https://s3.amazonaws.com/uifaces/faces/twitter/adhamdannaway/128.jpg",
+            time: "1 hour ago"
+        },
+        noti:" has send you a invitation to interview "
+    },
+    {
+        user:{
+            id:3,
+            name: "Sotatek",
+            avatar : "https://s3.amazonaws.com/uifaces/faces/twitter/adhamdannaway/128.jpg",
+            time: "1 hour ago"
+        },
+        noti:" has send you a invitation to interview "
+    },
+    {
+        user:{
+            id:4,
+            name: "Sotatek",
+            avatar : "https://s3.amazonaws.com/uifaces/faces/twitter/adhamdannaway/128.jpg",
+            time: "1 hour ago"
+        },
+        noti:" has send you a invitation to interview "
+    },
+    {
+        user:{
+            id:5,
+            name: "Sotatek",
+            avatar : "https://s3.amazonaws.com/uifaces/faces/twitter/adhamdannaway/128.jpg",
+            time: "1 hour ago"
+        },
+        noti:" has send you a invitation to interview "
+    },
+    {
+        user:{
+            id:6,
+            name: "Sotatek",
+            avatar : "https://s3.amazonaws.com/uifaces/faces/twitter/adhamdannaway/128.jpg",
+            time: "1 hour ago"
+        },
+        noti:" has send you a invitation to interview "
+    },
+    {
+        user:{
+            id:7,
+            name: "Sotatek",
+            avatar : "https://s3.amazonaws.com/uifaces/faces/twitter/adhamdannaway/128.jpg",
+            time: "1 hour ago"
+        },
+        noti:" has send you a invitation to interview "
+    },
+    {
+        user:{
+            id:8,
+            name: "Sotatek",
+            avatar : "https://s3.amazonaws.com/uifaces/faces/twitter/adhamdannaway/128.jpg",
+            time: "1 hour ago"
+        },
+        noti:" has send you a invitation to interview "
+    },
+    {
+        user:{
+            id:9,
+            name: "Sotatek",
+            avatar : "https://s3.amazonaws.com/uifaces/faces/twitter/adhamdannaway/128.jpg",
+            time: "1 hour ago"
+        },
+        noti:" has send you a invitation to interview "
+    },
+    {
+        user:{
+            id:10,
+            name: "Sotatek",
+            avatar : "https://s3.amazonaws.com/uifaces/faces/twitter/adhamdannaway/128.jpg",
+            time: "1 hour ago"
+        },
+        noti:" has send you a invitation to interview "
+    },
+    {
+        user:{
+            id:11,
+            name: "Sotatek",
+            avatar : "https://s3.amazonaws.com/uifaces/faces/twitter/adhamdannaway/128.jpg",
+            time: "1 hour ago"
+        },
+        noti:" has send you a invitation to interview "
+    },
+    {
+        user:{
+            id:12,
+            name: "Sotatek",
+            avatar : "https://s3.amazonaws.com/uifaces/faces/twitter/adhamdannaway/128.jpg",
+            time: "1 hour ago"
+        },
+        noti:" has send you a invitation to interview "
+    },
+    {
+        user:{
+            id:13,
+            name: "Sotatek",
+            avatar : "https://s3.amazonaws.com/uifaces/faces/twitter/adhamdannaway/128.jpg",
+            time: "1 hour ago"
+        },
+        noti:" has send you a invitation to interview "
+    },
+    {
+        user:{
+            id:14,
+            name: "Sotatek",
+            avatar : "https://s3.amazonaws.com/uifaces/faces/twitter/adhamdannaway/128.jpg",
+            time: "1 hour ago"
+        },
+        noti:" has send you a invitation to interview "
+    }
+];
+export default Notification;
