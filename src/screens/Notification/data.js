2bb448124f4cf2222d6861f63b53695248b3c453
sotatek-thiennguyen <thiennguyen@sotatek.com>
2019-05-09 17:33:29 +0700

    fixNoti

--- src/screens/Notification/data.js
+++ src/screens/Notification/data.js
@@ -5,135 +5,166 @@ var Notification = [
             id: 0,
             name: "SamSungRnD",
             avatar : "https://s3.amazonaws.com/uifaces/faces/twitter/adhamdannaway/128.jpg",
-            time: "1 hour ago"
+            time: "1 hour ago",
+            readed: false,            
         },
-        noti:" has approve your CV. "
+        noti:" has approve your CV. ",
+        category:"approve",type:"font-awesome",icon:"thumbs-up",color:"blue"
     },
     {
         user:{
+            readed: false,
             id:1,
             name: "Viettel Telecom",
             avatar : "https://s3.amazonaws.com/uifaces/faces/twitter/adhamdannaway/128.jpg",
             time: "1 hour ago"
         },
-        noti:" has send you a invitation to interview "
+        noti:" has send you a invitation to interview ",
+        category:"invite",icon:"send",color:"orange"
+
     },
     {
         user:{
+            readed: true,
             id:2,
             name: "Sotatek",
             avatar : "https://s3.amazonaws.com/uifaces/faces/twitter/adhamdannaway/128.jpg",
             time: "1 hour ago"
         },
-        noti:" has send you a invitation to interview "
+        noti:" has send you a invitation to interview ",
+        category:"invite",icon:"send",color:"orange"
     },
     {
         user:{
+            readed: false,
             id:3,
             name: "Sotatek",
             avatar : "https://s3.amazonaws.com/uifaces/faces/twitter/adhamdannaway/128.jpg",
             time: "1 hour ago"
         },
-        noti:" has send you a invitation to interview "
+        noti:" has send you a invitation to interview ",
+        category:"invite",icon:"send",color:"orange"
     },
     {
         user:{
+            readed: true,
             id:4,
             name: "Sotatek",
             avatar : "https://s3.amazonaws.com/uifaces/faces/twitter/adhamdannaway/128.jpg",
             time: "1 hour ago"
         },
-        noti:" has send you a invitation to interview "
+        noti:" has approve your CV. ",
+        category:"approve",type:"font-awesome",icon:"thumbs-up",color:"blue" 
     },
     {
         user:{
+            readed: true,
             id:5,
             name: "Sotatek",
             avatar : "https://s3.amazonaws.com/uifaces/faces/twitter/adhamdannaway/128.jpg",
             time: "1 hour ago"
         },
-        noti:" has send you a invitation to interview "
+        noti:" has send you a invitation to interview ",
+        category:"invite",icon:"send",color:"orange"
     },
     {
         user:{
+            readed: false,
             id:6,
             name: "Sotatek",
             avatar : "https://s3.amazonaws.com/uifaces/faces/twitter/adhamdannaway/128.jpg",
             time: "1 hour ago"
         },
-        noti:" has send you a invitation to interview "
+        noti:" has approve your CV. ",
+        category:"approve",type:"font-awesome",icon:"thumbs-up",color:"blue"
     },
     {
         user:{
+            readed: true,
             id:7,
             name: "Sotatek",
             avatar : "https://s3.amazonaws.com/uifaces/faces/twitter/adhamdannaway/128.jpg",
             time: "1 hour ago"
         },
-        noti:" has send you a invitation to interview "
+        noti:" has send you a invitation to interview ",
+        category:"invite",icon:"send",color:"orange"
     },
     {
         user:{
+            readed: true,
             id:8,
             name: "Sotatek",
             avatar : "https://s3.amazonaws.com/uifaces/faces/twitter/adhamdannaway/128.jpg",
             time: "1 hour ago"
         },
-        noti:" has send you a invitation to interview "
+        noti:" has send you a invitation to interview ",
+        category:"invite",icon:"send",color:"orange"
     },
     {
         user:{
+            readed: true,
             id:9,
             name: "Sotatek",
             avatar : "https://s3.amazonaws.com/uifaces/faces/twitter/adhamdannaway/128.jpg",
             time: "1 hour ago"
         },
-        noti:" has send you a invitation to interview "
+        noti:" has send you a invitation to interview ",
+        category:"invite",icon:"send",color:"orange"
     },
     {
         user:{
+            readed: true,
             id:10,
             name: "Sotatek",
             avatar : "https://s3.amazonaws.com/uifaces/faces/twitter/adhamdannaway/128.jpg",
             time: "1 hour ago"
         },
-        noti:" has send you a invitation to interview "
+        noti:" has send you a invitation to interview ",
+        category:"invite",icon:"send",color:"orange"
     },
     {
         user:{
+            readed: true,
             id:11,
             name: "Sotatek",
             avatar : "https://s3.amazonaws.com/uifaces/faces/twitter/adhamdannaway/128.jpg",
             time: "1 hour ago"
         },
-        noti:" has send you a invitation to interview "
+        noti:" has send you a invitation to interview ",
+        category:"invite",icon:"send",color:"orange"
     },
     {
         user:{
+            readed: true,
             id:12,
             name: "Sotatek",
             avatar : "https://s3.amazonaws.com/uifaces/faces/twitter/adhamdannaway/128.jpg",
             time: "1 hour ago"
         },
-        noti:" has send you a invitation to interview "
+        noti:" has send you a invitation to interview ",
+        category:"invite",icon:"send",color:"orange"
     },
     {
         user:{
+            readed: true,
             id:13,
             name: "Sotatek",
             avatar : "https://s3.amazonaws.com/uifaces/faces/twitter/adhamdannaway/128.jpg",
             time: "1 hour ago"
         },
-        noti:" has send you a invitation to interview "
+        noti:" has send you a invitation to interview ",
+        category:"invite",icon:"send",color:"orange"
     },
     {
         user:{
+            readed: true,
             id:14,
             name: "Sotatek",
             avatar : "https://s3.amazonaws.com/uifaces/faces/twitter/adhamdannaway/128.jpg",
             time: "1 hour ago"
         },
-        noti:" has send you a invitation to interview "
+        noti:" has send you a invitation to interview ",
+        category:"invite",icon:"send",color:"orange"
     }
 ];
 export default Notification;
