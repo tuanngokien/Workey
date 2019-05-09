fab188af86e327425fa923d5b50dff98002a47ff
sotatek-thiennguyen <thiennguyen@sotatek.com>
2019-05-09 17:33:29 +0700

    fixNoti

--- src/components/TopNavigator/index.js
+++ src/components/TopNavigator/index.js
@@ -1,6 +1,7 @@
 import TopNavigator from "./TopNavigator";
 import SimpleTopNavigator from "./SimpleTopNavigator";
+import TopNoti from "./TopNoti";
 
-
+export {TopNoti};
 export {SimpleTopNavigator};
 export default TopNavigator;
