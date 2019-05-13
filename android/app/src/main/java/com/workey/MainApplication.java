12de2d8f0ad8a07958af5b22896ea5c3542c9a2d
sotatek-thiennguyen <thiennguyen@sotatek.com>
2019-05-13 08:37:05 +0700

    pushnoti

--- android/app/src/main/java/com/workey/MainApplication.java
+++ android/app/src/main/java/com/workey/MainApplication.java
@@ -3,6 +3,7 @@ package com.workey;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.geektime.rnonesignalandroid.ReactNativeOneSignalPackage;
 import com.airbnb.android.react.maps.MapsPackage;
 import com.BV.LinearGradient.LinearGradientPackage;
 import com.horcrux.svg.SvgPackage;
@@ -30,6 +31,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new ReactNativeOneSignalPackage(),
             new MapsPackage(),
             new LinearGradientPackage(),
             new SvgPackage(),
