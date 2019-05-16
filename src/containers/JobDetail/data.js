a6de018110d686a098153876b648bb6ad53daca4
sotatek-thiennguyen <thiennguyen@sotatek.com>
2019-05-16 14:59:12 +0700

    fakeDataSearchResult

--- src/containers/JobDetail/data.js
+++ src/containers/JobDetail/data.js
@@ -1,11 +1,30 @@
 import JobCover from "../../assets/images/background/google.jpg";
-import Logo from "../../assets/images/logo/google.png";
+import ShopeeLogo from "../../assets/images/logo/shopee.png";
+import GoogleLogo from "../../assets/images/logo/google.png";
+import PayPalLogo from "../../assets/images/logo/paypal.png";
+import DropboxLogo from "../../assets/images/logo/dropbox.png";
+import SpotifyLogo from "../../assets/images/logo/spotify.png";
+import AmazonLogo from "../../assets/images/logo/amazon.png";
+import LinkedInBanner from "../../assets/images/banner/3.jpg";
+import LinkedInLogo from "../../assets/images/logo/linkedin.png";
+import ChromeBanner from "../../assets/images/banner/4.jpg";
+import ChromeLogo from "../../assets/images/logo/chrome.png";
+import FoursquareBanner from "../../assets/images/banner/5.jpg";
+import FoursquareLogo from "../../assets/images/logo/foursquare.png";
+import XingBanner from "../../assets/images/banner/7.jpg";
+import XingLogo from "../../assets/images/logo/xing.png";
+import VKBanner from "../../assets/images/banner/6.jpg";
+import VKLogo from "../../assets/images/logo/vk.png";
+import ShopeeCover from "../../assets/images/background/ShopeeCover.jpg";
+import AmazonCover from "../../assets/images/background/AmazonCover.jpg";
+import DropboxCover from "../../assets/images/background/DropboxCover.jpg";
+import PayPalCover from "../../assets/images/background/paypalCover.jpg";
 
 export default Data = [
     {
         id: 1,
         cover: JobCover,
-        logo: Logo,
+        logo: GoogleLogo,
         title: "UI / UX Designer",
         employer: "Google",
         createdAt: "April 15, 2019",
@@ -24,5 +43,215 @@ export default Data = [
             "Design, Graphic Arts Institute, BFA, MFA, etc. prefered",
             "Visual design experience a plus",
         ]
-    }
+    },
+    {
+        id: 2,
+        cover: ShopeeCover,
+        logo: ShopeeLogo,
+        title: "IT Systems Supervisor",
+        employer: "Shoppe",
+        createdAt: "May 01, 2019",
+        viewCount: "1680",
+        rating: "4.4",
+        address: "10th floor, Keangnam Landmark 72, Hanoi",
+        description: "Multiple computer operating systems; Analyzing and designing complex systems; Four years of progressively responsible programming and systems experience.",
+        responsibilities: [
+            "Daily tickets for support and maintenance of all public safety applications",
+            "Day to day operations for applications and field support",
+            "Prepare and review all personnel yearly appraisals.",
+            "Scheduling of all team members",
+        ],
+        qualifications: [
+            "Bachelor's Degree in a related field",
+            "Four years of progressively responsible programming and systems experience",
+            "Visual design experience a plus",
+        ]
+    },
+    {
+        
+            id: 3,
+            cover: AmazonCover,
+            logo: AmazonLogo,
+            title: "IT Tester",
+            employer: "Amazon",
+            createdAt: "January 18, 2019",
+            viewCount: "3280",
+            rating: "4.7",
+            address: "7th floor, MD Complex, 68 Nguyen Co Thach Hanoi",
+            description: "Achieve measurable business results in desktop support or strategic information services projects.",
+            responsibilities: [
+                "Estimate man-day and provide timeline for automating test scenario",
+                "Do Java coding to automate manual test case either for Web, Mobile, or API.",
+                "Prepare test environment, test data, jenkin configuration if require.",
+                "Run, analyze automation test report then send out to the team.",
+            ],
+            qualifications: [
+                "3+ years’ experience playing as Automation Tester position, having experience in developing test automation solutions.",
+                "Experience using technology of Selenium 2.0 (WebDriver, the Page Object Pattern) with Jbehave/Cucumber.",
+                "Experiences in automation testing on Mobile using Appium.",
+                "Experience with Java as a development language for automation"
+            ]
+        
+    },
+    {
+        id: 4,
+        cover: DropboxCover,
+        logo: DropboxLogo,
+        title: "C# Developer",
+        employer: "Dropbox",
+        createdAt: "May 17, 2019",
+        viewCount: "680",
+        rating: "4.7",
+        address: "131 Ho Tung Mau, Cau Giay, Hanoi",
+        description: "This position will present this individual with the opportunity to join a growing organization that is utilizing new cutting edge technology in an enterprise level company.",
+        responsibilities: [
+            "Daily tickets for support and maintenance of all public safety applications",
+            "Day to day operations for applications and field support",
+            "Prepare and review all personnel yearly appraisals.",
+            "Scheduling of all team members",
+        ],
+        qualifications: [
+            "Bachelor's Degree in a related field",
+            "Four years of progressively responsible programming and systems experience",
+            "Visual design experience a plus",
+        ]
+    },
+    {
+        id: 5,
+        cover: PayPalCover,
+        logo: PayPalLogo,
+        title: "Lead Software Engineer",
+        employer: "PayPal",
+        createdAt: "April 17, 2019",
+        viewCount: "680",
+        rating: "4.9",
+        address: "12 Nguyen Binh Khiem, Quan 1, HCM",
+        description: "Collaborate with Product Owners and Internal technology team owners to understand and evolve the product requirements to deliver high quality software products.",
+        responsibilities: [
+            "Estimate man-day and provide timeline for automating test scenario",
+            "Do Java coding to automate manual test case either for Web, Mobile, or API.",
+            "Prepare test environment, test data, jenkin configuration if require.",
+            "Run, analyze automation test report then send out to the team.",
+        ],
+        qualifications: [
+            "Bachelor's Degree in a related field",
+            "Four years of progressively responsible programming and systems experience",
+            "Visual design experience a plus",
+        ]
+    },
+    {
+        id: 6,
+        cover: LinkedInBanner,
+        logo: LinkedInLogo,
+        title: "Android Developer",
+        employer: "LinkedIn",
+        createdAt: "April 14, 2019",
+        viewCount: "6680",
+        rating: "4.7",
+        address: "10 floor, MHDI Complex, My Dinh, Hanoi",
+        description: "Collaborate with Product Owners and Internal technology team owners to understand and evolve the product requirements to deliver high quality software products.",
+        responsibilities: [
+            "Estimate man-day and provide timeline for automating test scenario",
+            "Do Java coding to automate manual test case either for Web, Mobile, or API.",
+            "Prepare test environment, test data, jenkin configuration if require.",
+            "Run, analyze automation test report then send out to the team.",
+        ],
+        qualifications: [
+            "Bachelor's Degree in a related field",
+            "Four years of progressively responsible programming and systems experience",
+            "Visual design experience a plus",
+        ]
+    },
+    {
+        id: 7,
+        cover: ChromeBanner,
+        logo: ChromeLogo,
+        title: "VR Sales",
+        employer: "Google",
+        createdAt: "May 17, 2019",
+        viewCount: "1680",
+        rating: "4.0",
+        address: "12 Nguyen Binh Khiem, Quan 1, HCM",
+        description: "Collaborate with Product Owners and Internal technology team owners to understand and evolve the product requirements to deliver high quality software products.",
+        responsibilities: [
+            "Estimate man-day and provide timeline for automating test scenario",
+            "Do Java coding to automate manual test case either for Web, Mobile, or API.",
+            "Prepare test environment, test data, jenkin configuration if require.",
+            "Run, analyze automation test report then send out to the team.",
+        ],
+        qualifications: [
+            "Bachelor's Degree in a related field",
+            "Four years of progressively responsible programming and systems experience",
+            "Visual design experience a plus",
+        ]
+    },
+    {
+        id: 8,
+        cover: FoursquareBanner,
+        logo: FoursquareLogo,
+        title: "Geographic Analyst",
+        employer: "Foursquare",
+        createdAt: "April 17, 2019",
+        viewCount: "2680",
+        rating: "4.5",
+        address: "12 Nguyen Binh Khiem, Quan 1, HCM",
+        description: "Collaborate with Product Owners and Internal technology team owners to understand and evolve the product requirements to deliver high quality software products.",
+        responsibilities: [
+            "Estimate man-day and provide timeline for automating test scenario",
+            "Do Java coding to automate manual test case either for Web, Mobile, or API.",
+            "Prepare test environment, test data, jenkin configuration if require.",
+            "Run, analyze automation test report then send out to the team.",
+        ],
+        qualifications: [
+            "Bachelor's Degree in a related field",
+            "Four years of progressively responsible programming and systems experience",
+            "Visual design experience a plus",
+        ]
+    },
+    {
+        id: 9,
+        cover: XingBanner,
+        logo: XingLogo,
+        title: "Ophthalmic Nurse",
+        employer: "Xing",
+        createdAt: "April 17, 2019",
+        viewCount: "1280",
+        rating: "4.9",
+        address: "12 Nguyen Binh Khiem, Quan 1, HCM",
+        description: "Collaborate with Product Owners and Internal technology team owners to understand and evolve the product requirements to deliver high quality software products.",
+        responsibilities: [
+            "Estimate man-day and provide timeline for automating test scenario",
+            "Do Java coding to automate manual test case either for Web, Mobile, or API.",
+            "Prepare test environment, test data, jenkin configuration if require.",
+            "Run, analyze automation test report then send out to the team.",
+        ],
+        qualifications: [
+            "Bachelor's Degree in a related field",
+            "Four years of progressively responsible programming and systems experience",
+            "Visual design experience a plus",
+        ]
+    },
+    {
+        id: 10,
+        cover: VKBanner,
+        logo: VKLogo,
+        title: "English Teacher",
+        employer: "VKCo",
+        createdAt: "April 17, 2019",
+        viewCount: "1290",
+        rating: "4.9",
+        address: "12 Nguyen Binh Khiem, Quan 1, HCM",
+        description: "Collaborate with Product Owners and Internal technology team owners to understand and evolve the product requirements to deliver high quality software products.",
+        responsibilities: [
+            "Estimate man-day and provide timeline for automating test scenario",
+            "Do Java coding to automate manual test case either for Web, Mobile, or API.",
+            "Prepare test environment, test data, jenkin configuration if require.",
+            "Run, analyze automation test report then send out to the team.",
+        ],
+        qualifications: [
+            "Bachelor's Degree in a related field",
+            "Four years of progressively responsible programming and systems experience",
+            "Visual design experience a plus",
+        ]
+    },    
 ];
